620d2808e3a349d31ffff2cff4ac126edeb26843
Piotr Setsko <lihadzey@gmail.com>
2019-12-29 16:25:59 +0300

    feat: add canvas size switch

---
+++ simple-piskel-clone/src/index.js
@@ -0,0 +1,261 @@
+import "./style.css";
+
+const canvas = document.querySelector('.canvas');
+const ctx = canvas.getContext('2d');
+const frameCanvas = document.querySelector('.frame-canvas');
+const ctx2 = frameCanvas.getContext('2d');
+ctx2.imageSmoothingEnabled = false;
+
+const DEFAULT_CANVAS_SIZE = 512;
+
+const clone = document.querySelector('.frame-clone');
+const listFrames = document.querySelector('.frames-list');
+const del = document.querySelector('.frame-delete');
+const item = document.querySelector('.frame-item');
+const newFrame = document.querySelector('.frame-new');
+const size32 = document.querySelector('.size-32');
+const size64 = document.querySelector('.size-64');
+const size128 = document.querySelector('.size-128');
+const canvasSection = document.querySelector('.canvas-section');
+
+let sizeCanvas = 32;
+canvas.width = sizeCanvas;
+canvas.height = sizeCanvas;
+
+canvasSection.addEventListener('click', ()=> {
+  console.log(event.target);
+  if (event.target === size64 && sizeCanvas !== 64) {
+    sizeCanvas = 64;
+    setCanvasSize(sizeCanvas);
+  } else if (event.target === size128 && sizeCanvas !== 128) {
+    sizeCanvas = 128;
+    setCanvasSize(sizeCanvas);
+  } else if (event.target === size32 && sizeCanvas !== 32) {
+    sizeCanvas = 32;
+    setCanvasSize(sizeCanvas);
+  }
+})
+
+function setCanvasSize(sizeCanvas) {
+  let delta;
+  let oldimg = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  delta = (sizeCanvas - canvas.width) / 2;
+  canvas.width = sizeCanvas;
+  canvas.height = sizeCanvas;
+  ctx.putImageData(oldimg, delta, delta);
+  ctx2.clearRect(0, 0, frameCanvas.width, frameCanvas.height);
+  ctx2.drawImage(canvas, 0, 0, canvas.width, canvas.height, 0, 0, frameCanvas.width, frameCanvas.height);
+}
+
+let i = 0
+newFrame.addEventListener('click', () => {
+  getFrame();
+});
+
+function getFrame() {
+  const frame = `<li class="frames-item item-${i}">
+      <canvas class="frame-canvas" width="100" height="100"></canvas>
+      <button class="frame-clone" onclick="getCloneFrame()" >clone</button>
+    </li>`
+    listFrames.insertAdjacentHTML("beforeend", frame);
+    i += 1;
+  }
+
+
+const list = document.querySelector('.instrument__list');
+const colorList = document.querySelector('.color__list');
+const picker = document.querySelector('.page-list__button--colorer');
+const bucket = document.querySelector('.page-list__button--bucket');
+const pencil = document.querySelector('.page-list__button--pencil');
+const red = document.querySelector('.page-list__legend--red');
+const blue = document.querySelector('.page-list__legend--blue');
+const previous = document.querySelector('.page-list__legend--prev');
+const current = document.querySelector('.page-list__button--current');
+
+let flattenedRGBAValues;
+let hexPixels;
+let prevColor = '#00ff00';
+
+
+const hexToRGBA = (hexStr) => [
+  parseInt(hexStr.substr(0, 2), 16),
+  parseInt(hexStr.substr(2, 2), 16),
+  parseInt(hexStr.substr(4, 2), 16),
+  255,
+];
+
+function rgbaToHEX(rgbaStr) {
+  const arr = rgbaStr;
+  arr[0] = arr[0].toString(16);
+  arr[1] = arr[1].toString(16);
+  arr[2] = arr[2].toString(16);
+  if (arr[0].length === 1) {
+    arr[0] = `0${arr[0]}`;
+  }
+  if (arr[1].length === 1) {
+    arr[1] = `0${arr[1]}`;
+  }
+  if (arr[2].length === 1) {
+    arr[2] = `0${arr[2]}`;
+  }
+
+  return `#${arr[0]}${arr[1]}${arr[2]}`;
+}
+
+function drawPicture() {
+  const imgData = new ImageData(Uint8ClampedArray
+    .from(flattenedRGBAValues), hexPixels.length, hexPixels.length);
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.imageSmoothingEnabled = false;
+
+  createImageBitmap(imgData).then((imgBitmap) => {
+    ctx.drawImage(imgBitmap, 0, 0, hexPixels.length, hexPixels.length,
+      0, 0, canvas.width, canvas.height);
+  });
+}
+
+/*
+const image = (prom) => {
+  prom.then((json) => {
+    hexPixels = json;
+    if (localStorage.paint == null) {
+      flattenedRGBAValues = hexPixels
+        .flat()
+        .map(hexToRGBA)
+        .flat();
+      drawPicture();
+    } else {
+      const dataURL = localStorage.getItem('paint');
+      const img = new Image();
+      img.src = dataURL;
+      img.onload = () => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, 0, 0, 4, 4, 0, 0, canvas.width, canvas.height);
+      };
+    }
+  });
+};
+
+const promise = fetch('../src/assets/data/4x4.json');
+promise.then(
+  (response) => image(response.json()),
+);
+*/
+
+list.addEventListener('click', (evt) => {
+  if (evt.target.classList.contains('page-list')) {
+    evt.preventDefault();
+  } else {
+    for (let i = 0; i < list.children.length; i += 1) {
+      list.children[i].classList.remove('active');
+    }
+    if (evt.target.classList.contains('page-list__item')) {
+      evt.target.classList.add('active');
+    } else {
+      evt.target.parentElement.classList.add('active');
+    }
+  }
+});
+
+function addCurentColor(elem) {
+  const rgba = elem.children[0].value;
+  current.value = rgba;
+  if (current.value !== prevColor) {
+    previous.children[0].value = prevColor;
+  }
+  prevColor = rgba;
+}
+
+colorList.addEventListener('click', (evt) => {
+  if (evt.target === red) {
+    addCurentColor(red);
+  } else if (evt.target === blue) {
+    addCurentColor(blue);
+  } else if (evt.target === previous) {
+    addCurentColor(previous);
+  }
+});
+
+current.addEventListener('input', (event) => {
+  if (current.value !== prevColor) {
+    previous.children[0].value = prevColor;
+  }
+  prevColor = event.target.value;
+});
+
+
+document.addEventListener('keydown', (evt) => {
+  for (let i = 0; i < list.children.length; i += 1) {
+    list.children[i].classList.remove('active');
+  }
+  if (evt.code === 'KeyB') {
+    bucket.parentElement.classList.add('active');
+  } else if (evt.code === 'KeyP') {
+    pencil.parentElement.classList.add('active');
+  } else if (evt.code === 'KeyC') {
+    picker.parentElement.classList.add('active');
+  } else if (evt.code === 'KeyD') {
+    localStorage.clear();
+  }
+});
+
+function pick(event) {
+  const x = event.layerX / 128;
+  const y = event.layerY / 128;
+  const pixel = ctx.getImageData(x, y, 1, 1);
+  const { data } = pixel;
+  const rgba = rgbaToHEX(Array.from(data));
+  current.value = rgba;
+  if (current.value !== prevColor) {
+    previous.children[0].value = prevColor;
+  }
+  prevColor = rgba;
+}
+
+function fill() {
+  ctx.beginPath();
+  ctx.fillStyle = current.value;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fill();
+}
+
+pencil.addEventListener('click', () => {
+  
+})
+
+function penDraw(e) {
+  const x = Math.floor(e.layerX / (DEFAULT_CANVAS_SIZE / canvas.width));
+  const y = Math.floor(e.layerY / (DEFAULT_CANVAS_SIZE / canvas.height));
+  ctx.fillStyle = current.value;
+  ctx.fillRect(x, y, 1, 1);
+}
+
+canvas.addEventListener('mousedown', (e) => {
+  if (picker.parentElement.classList.contains('active')) {
+    pick(e);
+  }
+  if (bucket.parentElement.classList.contains('active')) {
+    fill();
+  }
+});
+
+canvas.onmousedown = (e) => {
+  if (pencil.parentElement.classList.contains('active')) {
+    penDraw(e);
+    ctx2.drawImage(canvas, 0, 0, canvas.width, canvas.height, 0, 0, frameCanvas.width, frameCanvas.height);
+  }
+  canvas.onmousemove = (evt) => {
+    if (pencil.parentElement.classList.contains('active')) {
+      penDraw(evt);
+      ctx2.drawImage(canvas, 0, 0, canvas.width, canvas.height, 0, 0, frameCanvas.width, frameCanvas.height);
+    }
+  };
+
+  canvas.onmouseup = () => {
+    canvas.focus();
+    localStorage.setItem('paint', canvas.toDataURL());
+    canvas.onmousemove = null;
+  };
+};
+
---
+++ simple-piskel-clone/webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /('node_modules')/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract(
+          {
+            fallback: 'style-loader',
+            use: ['css-loader'],
+          },
+        ),
+      },
+      {
+        test: /\.(eot|gif|otf|png|ttf|woff)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader',
+        options: {
+          outputPath: 'assets/images',
+        },
+      },
+      {
+        test: /\.svg$/,
+        use: {
+            loader: 'svg-url-loader',
+            options: {
+                encoding: 'base64'
+            }
+        }
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ['dist'],
+    }),
+    new ExtractTextPlugin({ filename: 'style.css' }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new CopyWebpackPlugin([
+      { from: 'src/assets/images', to: 'assets/images' },
+    ]),
+  ],
+};
